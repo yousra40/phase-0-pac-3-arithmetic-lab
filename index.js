3d578098a629a21f8eeda0356eeabade6e16dd0e
yousra40 <yusraalbaghdady123456@gmail.com>
2024-05-18 03:16:50 +0300

    done

---
+++ index.js
@@ -0,0 +1,34 @@
+// Basic Math Functions
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// Increment and Decrement Functions
+function increment(n) {
+  return ++n;
+}
+
+function decrement(n) {
+  return --n;
+}
+
+// Parsing Numbers Functions
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
